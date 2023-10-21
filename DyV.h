bdb04c502a16ce3e277d4413b1985cac058bebc2
Pablo Utrillas Gonzalez <putrgon@upv.edu.es>
2023-10-21 14:03:29 +0200

    Añadida busqueda binaria INV

--- DyV.h
+++ DyV.h
@@ -20,3 +20,22 @@ int busBin(val valor, vector<val>& vect,int ini, int fin){
 		}
 	}
 }
+
+template<typename val2>
+
+int busBin_INV(val2 valor, vector<val2>& vect,int ini, int fin){
+        if(ini > fin){
+                return -1;
+        }
+        int posMedia = (ini + fin)/2;
+        if(vect[posMedia] == valor){
+                return posMedia;
+        }else{
+                if(vect[posMedia] < valor){
+                        return busBin_INV(valor,vect,ini,posMedia-1);
+                }else{
+			return busBin_INV(valor,vect,posMedia+1,fin);
+                }
+        }
+}
+
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -14,6 +14,17 @@ int main(){
 	vector<double> c = {1.0,1.5,2.0,2.2,3.5,5.0,6.5,7.2,9.11,10.3};
         cout << "Busqueda float 9.11 en posicion: "<<busBin(9.11,c,0,c.size()) << endl;
 
+	cout << endl << "Busqueda invertida:" << endl;
+	
+	vector<int> d = {10,9,8,7,6,5,4,3,2,1};
+        cout << "Busqueda entero 2 en posicion: "<<busBin_INV(2,d,0,d.size()) << endl;
+
+        vector<char> e = {'j','i','h','g','f','e','d','c','b','a'};
+        cout << "Busqueda char f en posicion: "<<busBin_INV('f',e,0,e.size()) << endl;
+
+        vector<double> f = {10.3,9.11,7.2,6.5,5.0,3.5,2.2,2.0,1.5,1.0};
+        cout << "Busqueda float 9.11 en posicion: "<<busBin_INV(9.11,f,0,f.size()) << endl;
+
         return 0;
 }
 
